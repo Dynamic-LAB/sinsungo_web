35cc94fe686f2cb84c8ff1a2981f670b460fd2e0
Ham-ZZi <hyunzia2@naver.com>
2021-05-12 19:20:56 +0900

    레시피 페이지 레이아웃 스타일 수정 및 다듬기 1. 반응형 페이지로 구현을 위한 수정 2. Recipe.css 일부 스타일 styled-components 로 스타일 수정 2. my.css 필요없는 요소 삭제

--- src/components/Recipe/Recipe.js
+++ src/components/Recipe/Recipe.js
@@ -2,112 +2,128 @@ import "./Recipe.css";
 import WhiteBox from "../common/WhiteBox"
 import styled from 'styled-components';
 import TagBox from './TagBox';
-import { MdSearch} from 'react-icons/md';
+import {MdSearch} from 'react-icons/md';
 import RecipeCard from "./RecipeCard";
 import React, {useState, useCallback} from 'react';
 // import ice from "../../assets/ice.svg";
 
-const Box1=styled(WhiteBox)`
-height:100%;
-font-weight:500;
-font-size:17px;
+const WhiteBoxTop = styled(WhiteBox)`
+  height: auto;
+  margin-bottom: 20px;
 `;
-const SearchBar=styled.input.attrs({
+const SearchBlock = styled.div`
+  display: flex;
+
+  .icon_search {
+    display: flex;
+    font-size: 2rem;
+    padding: 10px;
+  }
+`;
+const WhiteBoxRecipe = styled(WhiteBox)`
+  height: 100%;
+`;
+const RecipeBlock = styled.div`
+  overflow-y: auto; //스크롤
+`;
+const SearchBar = styled.input.attrs({
   type: 'text',
-  placeholder:'검색어를 입력해주세요',
+  placeholder: '검색어를 입력해주세요',
 })`
-font-size: 16px;
-    width:100%;
-    height: auto;
-    line-height: normal; 
-    padding: .8em .5em;
-    cursor: pointer;
-    border: none;
-    outline: none;
-    
-    @media only screen and (max-width: 978px) {
-        font-size: 20px;
-      }
+  font-size: 16px;
+  width: 100%;
+  height: auto;
+  cursor: pointer;
+  border: none;
+  outline: none;
+  border-radius: 10px;
+  @media only screen and (max-width: 978px) {
+    font-size: 14px;
+  }
 `;
-
+const RecipeTitle = styled.div`
+  display: flex;
+  padding: 10px 15px;
+  align-items: center;
+  font-size: 10px;
+  border-bottom: 1px solid #bbbbbb;
+`;
 const Recipe = () => {
-    const [modal, setModal] = useState(false);
-    const popUp=()=>{
-        setModal(true);
-    }
-   /* const CardSample = () => {
-        const names = ['백숙', '치킨', '통닭', '바베큐','1','2','3','2','3','2','3','2','3'];
-        const hasList = ['닭', '치즈', '양파'];
-        const noneList = ['물', '얼음'];
-        const recipeList = names.map((name,index) =>
-         
-            <div onClick={popUp} className="card" key={index}>
-                   <div>{index}</div> 
-                    <RecipeModal
-                    visible={modal}
-                    onConfirm={()=>setModal(false)}
-                    onCancel={()=>setModal(false)}
-                    />
-            <div className="card_inner">
-                <OriImg src={process.env.PUBLIC_URL + '/img.jpg'} alt="오류"/>
-                <Box2>
-                    <Box3>{name}</Box3>
-                    <Box4>
-                        <HasItem>냉장고 속 재료 │</HasItem> {hasList.map((n,_i)=>{return n+(_i<hasList.length-1?',':'')})}
-                        <br></br>
-                        <NoneItem>없는 재료 │</NoneItem> {noneList.map((n,_i)=>{return n+(_i<noneList.length-1?',':'')})}
-                    </Box4>
-                </Box2>
-            </div>
-        </div>);
-        
-        return (recipeList);
-        
-      };*/
-        return(
-        <main>
-            <div className="recipe__container">
-            <div className="searchCard">
-                        <div className="searchCard_inner1">
-                            <MdSearch  style={{'fontSize': '2.25rem'}} />
-                            <SearchBar/>
+  const [modal, setModal] = useState(false);
+  const popUp = () => {
+    setModal(true);
+  }
+  /* const CardSample = () => {
+       const names = ['백숙', '치킨', '통닭', '바베큐','1','2','3','2','3','2','3','2','3'];
+       const hasList = ['닭', '치즈', '양파'];
+       const noneList = ['물', '얼음'];
+       const recipeList = names.map((name,index) =>
 
-                        </div>
-                    </div>
-                <br></br>
-       
-                <Box1>   
-                    <div className="icon-recpie">레시피</div>
-                    <hr style={{
-                            'border': '1px solid #bbbbbb',
-                             'borderBottom': '0px'
-                        }}/>
-                    <div className="recipe__cards">
-                    <RecipeCard
-                    name={'백숙'}
-                    hasList = {['닭', '치즈', '양파']}
-                    noneList = {['물', '얼음']}
-                    />
-                      <RecipeCard
-                    name={'치킨'}
-                    hasList = {['닭', '치즈', '양파']}
-                    noneList = {['물', '얼음']}
-                    />   <RecipeCard
-                    name={'치킨'}
-                    hasList = {['닭', '치즈', '양파']}
-                    noneList = {['물', '얼음']}
-                    />
-                      <RecipeCard
-                    name={ '통닭'}
-                    hasList = {['닭', '치즈', '양파']}
-                    noneList = {['물', '얼음']}
-                    />
-                </div>
-                </Box1>
-                
- 
+           <div onClick={popUp} className="card" key={index}>
+                  <div>{index}</div>
+                   <RecipeModal
+                   visible={modal}
+                   onConfirm={()=>setModal(false)}
+                   onCancel={()=>setModal(false)}
+                   />
+           <div className="card_inner">
+               <OriImg src={process.env.PUBLIC_URL + '/img.jpg'} alt="오류"/>
+               <Box2>
+                   <Box3>{name}</Box3>
+                   <Box4>
+                       <HasItem>냉장고 속 재료 │</HasItem> {hasList.map((n,_i)=>{return n+(_i<hasList.length-1?',':'')})}
+                       <br></br>
+                       <NoneItem>없는 재료 │</NoneItem> {noneList.map((n,_i)=>{return n+(_i<noneList.length-1?',':'')})}
+                   </Box4>
+               </Box2>
+           </div>
+       </div>);
+
+       return (recipeList);
+
+     };*/
+  return (
+    <main>
+      <div className="recipe__container">
+        <WhiteBoxTop>
+          <SearchBlock>
+            <div className="icon_search"><MdSearch/></div>
+            <SearchBar/>
+          </SearchBlock>
+        </WhiteBoxTop>
+
+        <WhiteBoxRecipe>
+
+          <RecipeTitle>
+            <div className="icon__recipe"/>
+            <h2>레시피</h2>
+          </RecipeTitle>
+          <RecipeBlock>
+            <div className="recipe__cards">
+              <RecipeCard
+                name={'백숙'}
+                hasList={['닭', '치즈', '양파']}
+                noneList={['물', '얼음']}
+              />
+              <RecipeCard
+                name={'치킨'}
+                hasList={['닭', '치즈', '양파']}
+                noneList={['물', '얼음']}
+              /> <RecipeCard
+              name={'치킨'}
+              hasList={['닭', '치즈', '양파']}
+              noneList={['물', '얼음']}
+            />
+              <RecipeCard
+                name={'통닭'}
+                hasList={['닭', '치즈', '양파']}
+                noneList={['물', '얼음']}
+              />
             </div>
-        </main>
-    );
+          </RecipeBlock>
+        </WhiteBoxRecipe>
+      </div>
+    </main>
+  );
 }
 export default Recipe;
--- src/components/Recipe/RecipeCard.js
+++ src/components/Recipe/RecipeCard.js
@@ -1,106 +1,109 @@
 import "./Recipe.css";
-import Whitebox from "../common/WhiteBox"
+import WhiteBox from "../common/WhiteBox"
 import styled from 'styled-components';
 import TagBox from './TagBox';
-import { MdSearch} from 'react-icons/md';
+import {MdSearch} from 'react-icons/md';
 import RecipeModal from "./recipPopUp/RecipeModal"
 import React, {useState, useCallback} from 'react';
 // import ice from "../../assets/ice.svg";
-const Box2=styled.div`
-margin:10px;
-height:100%;
-width:100%;
-display: flex;
-flex-direction: column;
+
+const StyledWhiteBox = styled(WhiteBox)`
+  height: auto;
+  padding: 15px;
+  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.125);
+  cursor: pointer;
+  &:hover{
+    border: 2px dashed #bcbcbc;
+  }
 `;
-const Box3=styled(Whitebox)`
-padding:10px;
-margin-top:px;
-height:20px;
-width:96%;
-border-radius: 4px;
-box-shadow: 0px 2px 0px rgba(0, 0, 0, 0.08);
-font-weight:700;
-color: #393939;
-font-size: 16px;
+const TextBlock = styled.div`
+  margin: 10px;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+const Box4 = styled.div`
+  padding: 10px;
+  margin-top: 1px;
+  height: 60px;
+  width: 96%;
+  flex: 1;
+
+  font-weight: 500;
+  color: #000000;
+  font-size: 13px;
+  @media only screen and (max-width: 978px) {
+    font-size: 11px;
+  }
 `;
 
-const Box4=styled.div`
-padding:10px;
-margin-top:1px;
-height:60px;
-width:96%;
-flex: 1;
+const HasItem = styled.span`
+  color: #3c82d9;
+`;
+const NoneItem = styled.span`
+  color: #D93C3C;
+`;
 
-font-weight: 500;
-color: #000000;
-font-size: 13px;
-@media only screen and (max-width: 978px) {
+const OriImg = styled.img`
+  width: 160px;
+  border-radius: 10px;
+`;
+const ItemTitle = styled.div`
+  display: block; //내용이 칸 넘어가면 자동으로 줄바꿈
+  margin-left: 5px;
+  padding-bottom: 8px;
   font-size: 15px;
-}
+  font-weight: 700;
+  border-bottom: 1px dashed #bbb;
+  @media only screen and (max-width: 978px) {
+    font-size: 13px;
+  }
 `;
-
-const HasItem=styled.span`
-color: #3c82d9;
-`;
-const NoneItem=styled.span`
-color: #D93C3C;
-`;
-
-const OriImg=styled.img`
-width:160px; 
-border-radius: 10px;
-`;
-
 
 const RecipeCard = (props) => {
-    const [modal, setModal] = useState(false);
-    const onAddClick = () =>{
-        setModal(true);
-    };
-    const onCancel = () => {
-        setModal(false);
-    };
-    const onConfirm = () => {
-        setModal(false);
-        // onAdd();
-    }
-    const popUp=()=>{
-        onAddClick();
-    }
-        return(
-            <>
-           <div onClick={popUp} className="card">
-            <div className="card_inner">
-                <OriImg src={process.env.PUBLIC_URL + '/img.jpg'} alt="오류"/>
-                <Box2>
-                    <div style={{
-                        'marginLeft':'5px'
-
-                    }}>{props.name}</div>
-                    <hr style={{
-                             'marginTop':'5px',
-                             'borderTop': '1px dashed #bbb',
-                             'borderBottom': '1px dashed #fff'
-
-                        }}/>
-                    <Box4>
-                        <HasItem>냉장고 속 재료 │</HasItem> {props.hasList.map((n,_i)=>{return n+(_i<props.hasList.length-1?',':'')})}
-                        <br></br>
-                        <NoneItem>없는 재료 │</NoneItem> {props.noneList.map((n,_i)=>{return n+(_i<props.noneList.length-1?',':'')})}
-                    </Box4>
-                </Box2>
-            </div>
+  const [modal, setModal] = useState(false);
+  const onAddClick = () => {
+    setModal(true);
+  };
+  const onCancel = () => {
+    setModal(false);
+  };
+  const onConfirm = () => {
+    setModal(false);
+    // onAdd();
+  }
+  const popUp = () => {
+    onAddClick();
+  }
+  return (
+    <>
+      <StyledWhiteBox onClick={popUp}>
+        <div className="card_inner">
+          <OriImg src={process.env.PUBLIC_URL + '/img.jpg'} alt="오류"/>
+          <TextBlock>
+            <ItemTitle>{props.name}</ItemTitle>
+            <Box4>
+              <HasItem>냉장고 속 재료 │</HasItem> {props.hasList.map((n, _i) => {
+              return n + (_i < props.hasList.length - 1 ? ',' : '')
+            })}
+              <br></br>
+              <NoneItem>없는 재료 │</NoneItem> {props.noneList.map((n, _i) => {
+              return n + (_i < props.noneList.length - 1 ? ',' : '')
+            })}
+            </Box4>
+          </TextBlock>
         </div>
-        <RecipeModal
-                name={props.name}
-                visible={modal}
-                onConfirm={onConfirm}
-                onCancel={onCancel}
-            />
-        </>
-        );
-        
-      };
+      </StyledWhiteBox>
+      <RecipeModal
+        name={props.name}
+        visible={modal}
+        onConfirm={onConfirm}
+        onCancel={onCancel}
+      />
+    </>
+  );
+
+};
 
 export default RecipeCard;
