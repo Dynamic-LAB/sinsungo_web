161a5edb3f1590cd0fa9c7ead71405b0ab0db726
Ham-ZZi <hyunzia2@naver.com>
2021-05-12 16:59:59 +0900

    반응형 구현 및 오류 수정 1. 각 페이지에 스크린 크기가 조건 크기 이하면 sidebar 닫히도록 수정 2. Fridge.js 에 냉장고 카테고리 소제목 수정

--- src/components/common/Rightbar/Right.js
+++ src/components/common/Rightbar/Right.js
@@ -15,12 +15,14 @@ const WhiteBoxMember = styled(WhiteBox)`
 `;
 
 const NoticeBlock = styled.div`
+  display: block;
   align-items: center;
   justify-content: space-between;
   padding: 5px 15px;
 `;
 
 const NoticeItem = styled.div`
+  display: block;
   color: #393939;
   font-size: 12px;
   padding: 5px 0 5px 0;
@@ -33,39 +35,39 @@ const Spacer = styled.div`
 
 
 const Right = () => {
-    return (
-        <div className="right">
-            <div className="right__container">
-                <div className="right__cards">
+  return (
+    <div className="right">
+      <div className="right__container">
+        <div className="right__cards">
 
-                    {/*알림*/}
-                    <WhiteBoxNotice>
-                        <div className="notice_card_title">
-                            <div className="icon-notice"><MdNotificationsNone/></div>
-                            <h2>알림</h2>
-                        </div>
-                        <NoticeBlock>
-                            <NoticeItem>알림1</NoticeItem>
-                            <NoticeItem>알림2</NoticeItem>
-                        </NoticeBlock>
-                    </WhiteBoxNotice>
-
-                    {/*멤버*/}
-                    <WhiteBoxMember>
-                        <div className="member_card_title">
-                            <div className="icon-member"><MdSupervisorAccount/></div>
-                            <h2>냉장고멤버</h2>
-                            <Spacer/>
-                            <MemberAddButton/>
-                        </div>
-                        <div className="r_card_inner">
-                            <Member type='right'/>
-                        </div>
-                    </WhiteBoxMember>
-                </div>
+          {/*알림*/}
+          <WhiteBoxNotice>
+            <div className="notice_card_title">
+              <div className="icon-notice"><MdNotificationsNone/></div>
+              <h2>알림</h2>
             </div>
+            <NoticeBlock>
+              <NoticeItem>알림1</NoticeItem>
+              <NoticeItem>알림2</NoticeItem>
+            </NoticeBlock>
+          </WhiteBoxNotice>
+
+          {/*멤버*/}
+          <WhiteBoxMember>
+            <div className="member_card_title">
+              <div className="icon-member"><MdSupervisorAccount/></div>
+              <h2>냉장고멤버</h2>
+              <Spacer/>
+              <MemberAddButton/>
+            </div>
+            <div className="r_card_inner">
+              <Member type='right'/>
+            </div>
+          </WhiteBoxMember>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Right;
