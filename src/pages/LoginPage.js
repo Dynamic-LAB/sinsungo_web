32575863877d11dd24452e36657cea1dffed67ff
Ham-ZZi <hyunzia2@naver.com>
2021-05-20 22:53:54 +0900

    로그인페이지 효과 추가 1. 애니메이션 추가 [추가] 1. 신선고 로고 동영상

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -3,9 +3,34 @@ import KakaoLoginv from '../components/KakaoLoginv';
 import GoogleButton from '../components/GoogleButton'
 import styled from "styled-components";
 import logo from '../assets/sinsungo_logo.png'
+import logo_video from '../assets/sinsungo_logo.mp4';
 import UserLoginOrSignup from "../components/ForServer/UserLoginOrSignup"
 
+// VIDEO(서서히 없어지기)
+const VideoBlock = styled.div`
+  opacity: 1;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%,-50%);
+  .login_video {
+    width: 500px;
+    height: 500px;
+    animation: fadeout 1s 4s forwards;
+    @keyframes fadeout {
+      from {
+        opacity: 1;
+      }
+      to {
+        opacity: 0;
+      }
+    }
+  }
+`;
+
+// LOGO, LOGIN_BUTTON(없어졌다가 생기기)
 const LoginBlock = styled.div`
+  opacity: 0;
   position: absolute;
   top: 50%;
   left: 50%;
@@ -14,6 +39,15 @@ const LoginBlock = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  animation: fadein 1s 5s forwards;
+  @keyframes fadein {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
   @media only screen and (max-width: 1350px) {
     display: block;
     text-align: center;
@@ -23,7 +57,6 @@ const LoginBlock = styled.div`
     }
   }
 `;
-
 const LoginButton = styled.div`
   margin-left: 50px;
   float: right;
@@ -51,14 +84,18 @@ const LoginPage = (props) => {
 
   return (
     <>
+      <VideoBlock>
+        <video autoPlay preload muted playsInline className="login_video">
+          <source src={logo_video} type="video/mp4"/>
+        </video>
+      </VideoBlock>
+      <UserLoginOrSignup loginInfo={loginInfo} setLoginInfo={setLoginInfo} success={success}/>
       <LoginBlock>
-        <UserLoginOrSignup loginInfo={loginInfo} setLoginInfo={setLoginInfo} success={success}/>
-          <img className="login_logo" src={logo}/>
-
+        <img className="login_logo" src={logo} alt="신선고 로고"/>
         <LoginButton>
-          <KakaoLoginv GoMain={GoMain}></KakaoLoginv>
-          <div style={{height: '5px'}}></div>
-          <GoogleButton GoMain={GoMain}></GoogleButton>
+          <KakaoLoginv GoMain={GoMain}/>
+          <div style={{height: '5px'}}/>
+          <GoogleButton GoMain={GoMain}/>
         </LoginButton>
       </LoginBlock>
     </>
