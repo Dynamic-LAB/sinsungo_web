e9494caf514ac73e6c8ee44136befe5b44de3fda
[Ham-ZZi] <[hyunjia2@naver.com]>
2021-06-02 00:32:29 +0900

    RightBar 알림 레이아웃 구현 [수정] 1. Right.js 2. Right.css 3. NoticeList.js 4. NoticeItem.js

--- src/components/My/Notice/NoticeList.js
+++ src/components/My/Notice/NoticeList.js
@@ -3,18 +3,27 @@ import NoticeItem from "./NoticeItem";
 
 
 const NoticeList = ({notices, onRemove, type}) => {
-    return(
-        <>{type === 'my' && (
-          <>
-              {notices.map(notice => (
-                  <NoticeItem  notice={notice} onRemove={onRemove} type="my"/>
-                )
-              )}
-          </>
-        )}
-
+  return (
+    <>
+      {type === 'my' && (
+        <>
+          {notices.map(notice => (
+              <NoticeItem notice={notice} onRemove={onRemove} type="my"/>
+            )
+          )}
         </>
-    );
+      )}
+      {type === 'right' && (
+        <>
+          {notices.map(notice => (
+              <NoticeItem notice={notice} onRemove={onRemove} type="right"/>
+            )
+          )}
+        </>
+      )}
+
+    </>
+  );
 }
 
 export default NoticeList;
