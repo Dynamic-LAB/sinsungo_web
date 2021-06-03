d6ee4c5fa666c0060622be7e4d0442f5299823b4
[Ham-ZZi] <[hyunjia2@naver.com]>
2021-06-04 00:04:48 +0900

    멤버 추가 코드 복사 기능 구현 [수정] 1. MemberModal.js

--- src/components/common/Rightbar/Member/MemberModal.js
+++ src/components/common/Rightbar/Member/MemberModal.js
@@ -1,8 +1,8 @@
-import React, { useContext } from 'react';
+import React, {useContext, useRef, useState} from 'react';
 import styled from "styled-components";
-import WhiteBox from "../../WhiteBox";
 import {MdClose} from "react-icons/md";
 import {Context} from "../../../../MemberList"
+
 const Fullscreen = styled.div`
   position: fixed;
   z-index: 30;
@@ -22,14 +22,24 @@ const ModalBlock = styled.div`
   padding: 1rem;
   border-radius: 10px;
   box-shadow: 0 0 8px rgba(0, 0, 0, 0.125);
+
   h3 {
     font-size: 1rem;
     margin: 20px 5px;
   }
+
   .copy {
     display: flex;
     justify-content: flex-end;
   }
+  .success {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: green;
+    margin-right: 20px;
+  }
+
   @media only screen and (max-width: 380px) {
     width: 260px;
   }
@@ -42,6 +52,7 @@ const ModalTitle = styled.div`
     font-size: 1.5rem;
     margin-top: 0;
   }
+
   @media only screen and (max-width: 380px) {
     h2 {
       font-size: 1.2rem;
@@ -57,6 +68,7 @@ const StyledInput = styled.input`
   border: none;
   border-bottom: 1px solid black;
   font-family: 'Noto Sans KR', sans-serif;
+
   &:hover {
     border-bottom: 1px solid #3C82D9;
   }
@@ -74,6 +86,7 @@ const CopyButton = styled.button`
   cursor: pointer;
   font-family: 'Noto Sans KR', sans-serif;
   transition: .2s;
+
   &:hover {
     box-shadow: 0 4px 10px rgba(0, 0, 0, 0.125);
     background: #3C82D9;
@@ -94,34 +107,51 @@ const CloseButton = styled.button`
   background: none;
 `;
 
-const MemberModal  = ({ visible,
-                        onCancel,}) => {
-                          
-
-  const {state,dispatch}=useContext(Context);
+const MemberModal = ({
+                       visible,
+                       onCancel,
+                     }) => {
+  const {state, dispatch} = useContext(Context);
+  const [success, setSuccess] = useState(false);
+  const textInput = useRef();
+  const copy = () => {
+    const el = textInput.current
+    el.select()
+    document.execCommand("copy");
+    setSuccess(true);
+  };
   if (!visible) return null;
-    return(
-      <Fullscreen>
-        <ModalBlock>
-          <ModalTitle>
-            <h2>냉장고멤버</h2>
-            <Spacer/>
-            <CloseButton onClick={onCancel}>
-              <MdClose/>
-            </CloseButton>
-          </ModalTitle>
-          <h3>링크</h3>
-          <StyledInput
-            readOnly
-            value={state.MemberList.invite_key}
-          />
-          <div className="copy">
-            <CopyButton>복사</CopyButton>
-          </div>
-        </ModalBlock>
-      </Fullscreen>
+  return (
+    <Fullscreen>
+      <ModalBlock>
+        <ModalTitle>
+          <h2>냉장고멤버</h2>
+          <Spacer/>
+          <CloseButton onClick={onCancel}>
+            <MdClose/>
+          </CloseButton>
+        </ModalTitle>
+        <h3>링크</h3>
+        <StyledInput
+          readOnly
+          value={state.MemberList.invite_key}
+          ref={textInput}
+        />
+        <div className="copy">
+          {
+            success ?
+              <div className="success">
+                Success!
+              </div> : null
+          }
+          <CopyButton onClick={copy}>복사</CopyButton>
 
-    );
+        </div>
+
+      </ModalBlock>
+    </Fullscreen>
+
+  );
 };
 
 export default MemberModal;
